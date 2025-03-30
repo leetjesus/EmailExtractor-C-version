26213fb3315b26e7ca3c338ffda2d0d6fdd8e040
leetjesus <JesusSecc@protonmail.com>
2025-03-30 14:04:35 -0700

    snippet code

--- main.c
+++ main.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// Get the user input using argv 
+// Auto-generate dataset file
+
+// Give the file name here
+int main(int argc, char *argv[]) {
+
+    FILE *file = fopen("intimshop.ru.sql", "r");
+
+    int file_count = NULL;
+
+
+    if (file == NULL) {
+        perror("Error opening a file.");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("File is now opened..\n");
+    
+
+    // Only get a section of the file
+    char buffer[100];
+
+    while(fgets(buffer, sizeof(buffer), file) != NULL){
+        printf("Read: %s", buffer); // Print each line
+    }
+
+    fclose(file);
+    return 0;
+
+
+
+}
