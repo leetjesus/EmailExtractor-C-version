c649c7f83a06e8d57996f6c61d97fc258f0606ce
leetjesus <JesusSecc@protonmail.com>
2025-03-30 15:27:44 -0700

    added argv for any type of dump file

--- main.c
+++ main.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Get the user input using argv 
-// Auto-generate dataset file
-
-// Give the file name here
 int main(int argc, char *argv[]) {
+    
+    if (argc <= 2) {
+        printf("Usage: %s <datadump_filename>\n", argv[0]);
+        return 1;
+    } else if (argc > 3){
+        printf("To many command-line arguments specified\n");
+        exit(EXIT_FAILURE);
+    }
 
-	// Datadump file
-    FILE *file = fopen("intimshop.ru.sql", "r");
+    FILE *file = fopen(argv[1], "r");
 
     char buffer_character;
     
     if (file == NULL) {
-        perror("Error opening a file.");
+        perror("Error opening a file.\n");
         exit(EXIT_FAILURE);
     }
 
+    // Reading the file
     while ((buffer_character = fgetc(file)) != EOF) {
         // so tech buffer_character represents a line 
-        printf("%c\n", buffer_character);
+        printf("%c", buffer_character);
     }
 
     fclose(file);
     return 0;
-
-
-
 }
